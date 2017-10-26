caba030f6e7582efed29da712405963dd4208022
Subodh Yadav <subyadav@egencia.com>
2017-10-27 01:01:39 +0530

    changes to server

--- index.js
+++ index.js
@@ -3,13 +3,59 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var numUsers = 0;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  var addedUser = false;
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    // io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', {
+      username: socket.username,
+      message: msg
+    });
+  });
+
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
 
